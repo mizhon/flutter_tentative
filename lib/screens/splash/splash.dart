f43d196c23dd9b61ec75d3ab7b803133d4c45553
mizhon <mizhon@live.cn>
2019-08-13 12:05:33 +0800

    添加毛玻璃效果

--- lib/screens/splash/splash.dart
+++ lib/screens/splash/splash.dart
@@ -1,6 +1,9 @@
 import 'dart:async';
+import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:flutter_tentative/screens/counter/counter.dart';
+// import 'package:flutter_tentative/screens/counter/counter.dart';
+import 'package:flutter_tentative/screens/splash/scaleRoute.dart';
 
 class SplashPage extends StatefulWidget {
   SplashPage();
@@ -12,13 +15,19 @@ class SplashPage extends StatefulWidget {
 class _SplashPageState extends State<SplashPage> {
 
   startTime() async {
-    var _duration = Duration(seconds: 3);
+    var _duration = Duration(seconds: 5);
     return Timer(_duration, _navigationPage);
   }
 
   void _navigationPage() {
     // Navigator.of(context).pushReplacementNamed('/Home');
-    Navigator.pushReplacement(context, MaterialPageRoute(builder: (BuildContext context) => CounterPage(title: 'Flutter Tentative')));
+    Navigator.pushReplacement(
+      context, 
+      // MaterialPageRoute(
+      //   builder: (BuildContext context) => CounterPage(title: 'Flutter Tentative')
+      // )
+      ScaleRoute(widget: CounterPage(title: 'Flutter Tentative'))
+    );
   }
 
   @override
@@ -31,10 +40,36 @@ class _SplashPageState extends State<SplashPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
+        fit: StackFit.expand,
         children: <Widget>[
-          Container(
-            
-          )
+          FlutterLogo(),
+          Center(
+            // child: Container(
+            //   child: Icon(
+            //     Icons.sentiment_satisfied,
+            //     size: 100.0,
+            //     color: Colors.blue
+            //   ),
+            // )
+            child: ClipRect(
+              child: BackdropFilter(
+                filter: ImageFilter.blur(sigmaX: 10.0, sigmaY: 10.0),
+                child: Container(
+                  width: 350.0,
+                  height: 350.0,
+                  decoration: BoxDecoration(
+                    color: Colors.grey.shade300.withOpacity(0.5)
+                  ),
+                  child: Center(
+                    child: Text(
+                      'Frosted',
+                      style: Theme.of(context).textTheme.display3,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
         ],
       ),
     );
